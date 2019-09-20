0eb05017d1bd769ab265cc902a5ed5ae2af95226
Tenebrae <TenebraeX8@gmx.at>
2019-09-20 13:07:53 +0200

    commented, added getAvailableNetworks

--- WLAN/Client.py
+++ WLAN/Client.py
@@ -1,5 +1,6 @@
-import sys
 import os
+import sys
+sys.path.append(os.path.dirname(__file__))
 import json
 from __core import executeAndGetLines
 from __core import executeNonQuery
@@ -9,6 +10,9 @@ from __core import xmlToDict
 #List Profiles
 
 def listProfiles():
+    '''
+    returns a stringlist with the wlan-profiles saved on this computer
+    '''
     out = executeAndGetLines("netsh wlan show profiles")
     OutStructure = __buildProfileStructure(out)
     print(OutStructure)
@@ -32,8 +36,11 @@ def __buildProfileStructure(lines):
     return json.dumps({Heading:Structure})
 
 #----------------------------------------------------------------------
-#save profile config
+#profile config
 def getProfileConfigurations():
+    '''
+    returns a list of JSON-encoded strings containing the advanced data of each profile on this computer
+    '''
     executeNonQuery("netsh wlan export profile")
     files = os.listdir(".")
     files = [x for x in files if x.endswith(".xml")]
@@ -48,6 +55,9 @@ def getProfileConfigurations():
     return profiles
 
 def getProfileConfiguration(strProfile):
+    '''
+    returns a JSON-encoded string containing the advanced data of the specified Profile
+    '''
     executeNonQuery("netsh wlan export profile " + strProfile)
     files = os.listdir(".")
     files = [x for x in files if x.endswith(".xml")]
@@ -64,13 +74,44 @@ def getProfileConfiguration(strProfile):
 #----------------------------------------------------------------------
 #disconnect
 def disconnect():
+    '''
+    shuts down the WLAN-Connection
+    '''
     executeNonQuery("netsh wlan disconnect")
 
 #-----------------------------------------------------------------------
 #connect
 def connectTo(strProfile):
+    '''
+    starts up a WLAN-Connection to the specified Profile
+    '''
     executeNonQuery("netch wlan connect " + strProfile)
 
-getProfileConfiguration("Catalysts")
+#------------------------------------------------------------------------
+#available profiles
+def getAvilableNetworks():
+    '''
+    returns an JSON-encoded list of Strings containing information of available Networks
+    '''
+    output = executeAndGetLines("netsh wlan show networks")
+    Wifis = []
+    output = output[1:]         #Skip interface name
+    while output[0].find(":") < 0: output = output[1:]
+    while len(output) >= 4:
+        key, name = __getValuePairFromLine(output[0])
+        if key != None:
+            Structure = {key:{}}
+            Structure[key]["name"] = name
+            for x in range(1,4):
+                attr, value = __getValuePairFromLine(output[x])
+                Structure[key][attr] = value
+            Wifis.append(json.dumps(Structure))
+        output = output[4:]
+    return Wifis
 
 
+def __getValuePairFromLine(strLine):
+    splitter = strLine.split(":")
+    if len(splitter) == 2:
+        return splitter[0].strip(), splitter[1].strip()
+    else: return None, None
